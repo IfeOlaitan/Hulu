2e96ae9f99d973b5605c40e78dab1173fbccf419
Ifeoluwa Olaiya <ifeoluwa1201@gmail.com>
2021-07-17 02:49:01 +0100

    update

---
+++ dist/js/main.js
@@ -0,0 +1,20 @@
+const modal = document.querySelector('.modal');
+const loginBtn = document.querySelector('.login-btn');
+const close = document.querySelector('.close');
+
+
+loginBtn.addEventListener('click', openModal);
+close.addEventListener('click', closeModal);
+window.addEventListener('click', outsideClick);
+
+function openModal() {
+    modal.style.display = 'block';
+}
+
+function closeModal() {
+    modal.style.display = 'none';
+}
+
+function outsideClick(e) {
+    if (e.target === modal) closeModal();
+}
